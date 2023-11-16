779eb91aad38a651c4faccadd656c49d7009b721
Dragica Babic <dragicababic80@gmail.com>
2023-11-16 15:05:08 +0100

    testing header

--- packages/androidApp/App.js
+++ packages/androidApp/App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Pressable, ImageBackground, Text, Image} from 'react-native';
+import { StyleSheet, View, Pressable, ImageBackground, Text, Image } from 'react-native';
 import LoginPage from '../app/src/components/LoginPage';
 import Items from '../app/src/components/Items';
 import ItemDetails from '../app/src/components/ItemDetails';
@@ -7,111 +7,118 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useDispatch } from "react-redux";
 import AllOffers from '../app/src/components/AllOffers';
-import {logout} from '../app/src/redux/slices/userSlice'
+import { logout } from '../app/src/redux/slices/userSlice'
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const dispatch=useDispatch();
-  const authenticated=useSelector(state=>state.users).authenticated
-  if(!authenticated){
-  return (        
-                <NavigationContainer>
-                  <Stack.Navigator>
-                    <Stack.Screen name="Login" component={LoginPage} options={{title: 'WebShop',
-          headerStyle: {
-            backgroundColor: '#0e4a38',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-          headerRight: () => (
-            <View style={{flex: 1}}>
-              <Text>TergtrhvehbrhrhTT</Text>
-              
-            <Pressable style={{marginRight:15}}>
-                    <ImageBackground source={require('../app/src/assets/baseline_list_white_24dp.png')}
-                       style={styles.image} resizeMode="cover" />
-                       <Text>Prvo dugme</Text>
-                </Pressable>
-                <Pressable onPress={()=>dispatch(logout())}>
-                    <ImageBackground source={require('../app/src/assets/baseline_logout_white_24dp.png')}
-                   style={styles.image} resizeMode="cover" />
-                   <Text>Proba</Text>
-                </Pressable>
-                </View>
-            )
-            }}/>
-                  </Stack.Navigator>
-                </NavigationContainer>
+  const dispatch = useDispatch();
+  const authenticated = useSelector(state => state.users).authenticated
+  if (!authenticated) {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={LoginPage} options={{
+            title: 'WebShop',
+            headerStyle: {
+              backgroundColor: '#0e4a38',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+            headerRight: () => (
+              <View style={{ flex: 1 }}>
+                <Text>TergtrhvehbrhrhTT</Text>
 
-  );
+                <Pressable style={{ marginRight: 15 }}>
+                  <ImageBackground source={require('../app/src/assets/baseline_list_white_24dp.png')}
+                    style={styles.image} resizeMode="cover" />
+                  <Text>Prvo dugme</Text>
+                </Pressable>
+                <Pressable onPress={() => dispatch(logout())}>
+                  <ImageBackground source={require('../app/src/assets/baseline_logout_white_24dp.png')}
+                    style={styles.image} resizeMode="cover" />
+                  <Text>Proba</Text>
+                </Pressable>
+              </View>
+            )
+          }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+
+    );
   }
-  else{
-    return(
-    <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen name="Items" component={Items} options={{title: 'WebShop',
-          headerStyle: {
-            backgroundColor: '#0e4a38',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-          headerRight: () => (
-                <Pressable onPress={()=>dispatch(logout())}>
-                    <ImageBackground  source={require('../app/src/assets/baseline_logout_white_24dp.png')}
-                    resizeMode="cover" style={{height:40, width:40}}/>
-                </Pressable>
-                
-            ),}}/>
-      <Stack.Screen name="Details" component={ItemDetails} options={{title: 'WebShop',
-          headerStyle: {
-            backgroundColor: '#0e4a38',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-          headerRight: () => (
-            <View>
-            <Pressable style={{marginRight:15}} onPress={()=>navigation.navigate('Offers')}>
-                    <ImageBackground source={require('../app/src/assets/baseline_list_white_24dp.png')}
-                        resizeMode="cover" style={styles.image} />
-                </Pressable>
-                <Pressable onPress={()=>dispatch(logout())}>
-                    <ImageBackground source={require('../app/src/assets/baseline_logout_white_24dp.png')}
+  else {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Items" component={Items} options={{
+            title: 'WebShop',
+            headerStyle: {
+              backgroundColor: '#0e4a38',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+            headerRight: () => (
+              <Pressable onPress={() => dispatch(logout())}>
+                <ImageBackground source={require('../app/src/assets/baseline_logout_white_24dp.png')}
+                  resizeMode="cover" style={{ height: 40, width: 40 }} />
+              </Pressable>
+
+            ),
+          }} />
+          <Stack.Screen name="Details" component={ItemDetails} options={{
+            title: 'WebShop',
+            headerStyle: {
+              backgroundColor: '#0e4a38',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+            headerRight: () => (
+              <View>
+                <Pressable style={{ marginRight: 15 }} onPress={() => navigation.navigate('Offers')}>
+                  <ImageBackground source={require('../app/src/assets/baseline_list_white_24dp.png')}
                     resizeMode="cover" style={styles.image} />
                 </Pressable>
-                </View>
-            ),}}/>
-            <Stack.Screen name="Offers" component={AllOffers} options={{title: 'WebShop',
-          headerStyle: {
-            backgroundColor: '#0e4a38',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-          headerRight: () => (
-            <View style={{flex:1, flexDirection:'row'}}>
-              <Text>TTT</Text>
-              
-            <Pressable style={{marginRight:15}}>
-                    <Image source={require('../app/src/assets/baseline_list_white_24dp.png')}
-                       style={{height:'30', width: '30'}} />
+                <Pressable onPress={() => dispatch(logout())}>
+                  <ImageBackground source={require('../app/src/assets/baseline_logout_white_24dp.png')}
+                    resizeMode="cover" style={styles.image} />
                 </Pressable>
-                <Pressable onPress={()=>dispatch(logout())}>
-                    <ImageBackground source={require('../app/src/assets/baseline_logout_white_24dp.png')}
-                   style={styles.image} />
-                   <Text>Proba</Text>
+              </View>
+            ),
+          }} />
+          <Stack.Screen name="Offers" component={AllOffers} options={{
+            title: 'WebShop',
+            headerStyle: {
+              backgroundColor: '#0e4a38',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+            headerRight: () => (
+              <View style={{ flex: 1, flexDirection: 'row' }}>
+                <Text>TTT</Text>
+
+                <Pressable style={{ marginRight: 15 }}>
+                  <Image source={require('../app/src/assets/baseline_list_white_24dp.png')}
+                    style={{ height: '30', width: '30' }} />
                 </Pressable>
-                </View>
-            ),}}/>
-    </Stack.Navigator>
-  </NavigationContainer>
+                <Pressable onPress={() => dispatch(logout())}>
+                  <ImageBackground source={require('../app/src/assets/baseline_logout_white_24dp.png')}
+                    style={styles.image} />
+                  <Text>Proba</Text>
+                </Pressable>
+              </View>
+            ),
+          }} />
+        </Stack.Navigator>
+      </NavigationContainer>
     )
   }
 }
@@ -123,8 +130,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  image:{
-    height:30,
-    width:30
+  image: {
+    height: 30,
+    width: 30
   }
 });
